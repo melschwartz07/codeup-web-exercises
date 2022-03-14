56a3e2c66a46c27cffc1912053e4e97a5a2af392
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-03-14 14:09:55 -0500

    js and html exercises

---
+++ js/js-with-html-practice.js
@@ -0,0 +1,7 @@
+var = myName = "Melanie";
+var myAge = 29;
+var isMonday = true;
+console.log(myName, myAge, isMonday);
+console.log( "I'm printing my name here: ", myName);
+console.log("my name: " + myName + " my age: " + myAge + " is it" +
+        "monday?" + isMonday); //concatenation

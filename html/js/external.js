eda5ab5023c4e622e80b9f471ae8d5ceccfbaa7b
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-03-26 15:38:37 -0500

    refactored files into appropriate directories

---
+++ html/js/external.js
@@ -0,0 +1,7 @@
+alert("Welcome to my Website!");
+
+var userInput = prompt("What is your favorite color?")
+console.log("The user entered: " + userInput);
+
+alert(userInput + " is my favorite color, too!")
+

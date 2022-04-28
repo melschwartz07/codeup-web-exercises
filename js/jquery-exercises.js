9cbad6d99f01ca597634e34d4cb6567ee755649b
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-04-28 14:04:23 -0500

    jquery mouse events commit

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,5 +1,19 @@
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
-});
-var welcome = $('#welcome').html()
-alert(welcome);
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+// var welcome = $('#welcome').html()
+// alert(welcome);
+//
+// var unorderedList = $('#list-item').html()
+// alert(unorderedList);
+
+
+
+// $('.codeup').css('background-color', 'red');
+
+// MOUSE ELEMENTS EXERCISES
+
+$('.header').css('visibility', 'visible')
+$('#click').click(function (){
+    $('#example').css('background', 'orange')
+})
